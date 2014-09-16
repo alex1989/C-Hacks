055bfd8dd6121084b7f7cc3abc1e267a9839ef8c
alex1989 <alex2007v@gmail.com>
2014-09-16 14:19:50 +0900

    Update MemoryLogger.hpp

--- MemoryLogger.hpp
+++ MemoryLogger.hpp
@@ -1,3 +1,6 @@
+#ifndef MEMORY_LOGGER_HPP
+#define MEMORY_LOGGER_HPP
+
 class MemoryLogger {
     static void* operator new(size_t size) throw(bad_alloc) { return ::operator new(size); }
     static void* operator new(size_t size, void * ptr) throw(bad_alloc) { return ::operator new(size, ptr); }
@@ -7,3 +10,5 @@ class MemoryLogger {
     static void operator delete(void * pMemory, void * ptr) throw() { ::operator delete(pMemory, ptr); }
     static void operator delete(void * pMemory, const nothrow_t& nt) throw() { return ::operator delete(pMemory, nt); }
 };
+
+#endif //MEMORY_LOGGER_HPP
